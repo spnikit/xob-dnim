cf1622cee330005576aa92b8e8a71e2f83af2797
spnikit <spnikit@gmail.com>
2023-09-06 23:19:25 +0300

    feat: add header

--- src/App.tsx
+++ src/App.tsx
@@ -1,45 +1,39 @@
-import {Input} from 'antd';
-import {useTodoList} from "./hooks/useTodoList";
-import {useTodoFilter} from "./hooks/useTodoFilter";
-import {filterOptions, initialState} from "./components/constants";
-import {TodoList} from "./components/TodoList";
-import {TodoControls} from "./components/TodoControls";
-import {TodoLayout} from "./components/TodoLayout";
+import { Input } from 'antd';
+import { useTodoList } from './hooks/useTodoList';
+import { useTodoFilter } from './hooks/useTodoFilter';
+import { filterOptions, initialState } from './components/constants';
+import { TodoList } from './components/TodoList';
+import { TodoControls } from './components/TodoControls';
+import { TodoLayout } from './components/TodoLayout';
 import './App.css';
 
 function App() {
-    const {filterOption, handleFilterOptionChange} = useTodoFilter(filterOptions[0]);
-    const {
-        itemsLeft,
-        handleClearCompleted,
-        handleInputEnter,
-        handleTodoClick,
-        list
-    } = useTodoList(initialState, filterOption);
+  const { filterOption, handleFilterOptionChange } = useTodoFilter(filterOptions[0]);
+  const { itemsLeft, handleClearCompleted, handleInputEnter, handleTodoClick, list } = useTodoList(
+    initialState,
+    filterOption,
+  );
 
-
-    return (
-        <TodoLayout>
-            <Input placeholder="What needs to be done?" onPressEnter={handleInputEnter}
-                   allowClear={true}/>
-            <TodoList list={list} onItemClick={handleTodoClick}/>
-            <TodoControls
-                filterOption={filterOption} itemsLeft={itemsLeft}
-                onFilterOptionChange={handleFilterOptionChange}
-                onClearCompleted={handleClearCompleted}
-            />
-        </TodoLayout>
-    )
+  return (
+    <TodoLayout>
+      <Input placeholder="What needs to be done?" onPressEnter={handleInputEnter} allowClear={true} />
+      <TodoList list={list} onItemClick={handleTodoClick} />
+      <TodoControls
+        filterOption={filterOption}
+        itemsLeft={itemsLeft}
+        onFilterOptionChange={handleFilterOptionChange}
+        onClearCompleted={handleClearCompleted}
+      />
+    </TodoLayout>
+  );
 }
 
-export default App
-
+export default App;
 
 /*
-* add header
-* make clear button disabled if no completed items
-* try to make input clear after enter
-* maybe add button to input
-* add min height
-* add strike strough to completed
-* */
+ * make clear button disabled if no completed items
+ * try to make input clear after enter
+ * maybe add button to input
+ * add min height
+ * add strike strough to completed
+ * */
--- src/components/TodoLayout/index.tsx
+++ src/components/TodoLayout/index.tsx
@@ -1,4 +1,4 @@
-import { Layout, Space } from 'antd';
+import { Layout, Space, Typography } from 'antd';
 import { ReactNode } from 'react';
 
 interface TodoLayoutProps {
@@ -6,10 +6,14 @@ interface TodoLayoutProps {
 }
 
 export const TodoLayout = ({ children }: TodoLayoutProps) => {
+  const { Text, Title } = Typography;
   return (
     <Layout>
       <Layout.Content style={{ margin: '0 auto', padding: '10px' }}>
         <Space direction="vertical" style={{ marginTop: '20px' }} size="middle">
+          <Title className="title" type="secondary">
+            ToDos
+          </Title>
           {children}
         </Space>
       </Layout.Content>
