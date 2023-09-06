82402812b7b82857283f7459f56690a26d8840ce
spnikit <spnikit@gmail.com>
2023-09-06 18:04:31 +0300

    chore: fix vite config

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,22 @@
 /// <reference types="vitest" />
-import { defineConfig } from 'vite'
+import {defineConfig} from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react({fastRefresh: false})],
-    test: {
-        globals: true,
-        environment: 'happy-dom',
-        setupFiles: ['src/setupTest.ts']
-    },
+export default defineConfig(({command}) => {
+
+    const config = {
+        plugins: [react({fastRefresh: false})],
+        base: '/',
+        test: {
+            globals: true,
+            environment: 'happy-dom',
+            setupFiles: ['src/setupTest.ts']
+        },
+    }
+
+    if (command !== 'serve') {
+        config.base = '/xob-dnim/'
+    }
+    return config;
 })
